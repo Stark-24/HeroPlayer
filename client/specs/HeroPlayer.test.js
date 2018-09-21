bff362e7c19cbb0184af6e7f24bdd74a097a2c52
Julie Im <julieim@Julies-MacBook-Air.local>
2018-09-20 19:03:56 -0700

    started on test

--- client/specs/HeroPlayer.test.js
+++ client/specs/HeroPlayer.test.js
@@ -1,15 +1,22 @@
-import React, {Component} from "react";
-import renderer from 'react-test-renderer';
+import React, {Component} from 'react';
 import { shallow } from "enzyme";
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import App from "../App.jsx";
+import {expect} from "chai";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import HeroPlayer from "../Components/HeroPlayer.jsx"
+import PlayButton from "../Components/PlayButton.jsx"
+//import App from "../App.jsx"
 
-Enzyme.configure({ adapter: new Adapter()});
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("HeroPlayer", () => {
-  it("should render correctly in debug mode", () => {
-    const component = renderer.create(<App />).toJSON();
-    expect(component).toMatchSnapshot();
+  // beforeEach(() => {
+  //   const component = 
+  // })
+  it("renders the HeroPlayer", () => {
+    const wrapper = shallow(<HeroPlayer />);
+    expect(wrapper.find(PlayButton)).to.have.lengthOf(1);
+
   });
 });
